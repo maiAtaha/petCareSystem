00190d56b292e204b3b0da2aea54666e454820f6
Taha <ktaha@morni.com>
2025-05-24 21:57:58 +0300

    add swagger

--- src/server.js
+++ src/server.js
@@ -35,5 +35,5 @@ app.use("/api/auth", authRoutes);
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
