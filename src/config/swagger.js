55b051c0ceaf5aee9afc3c2de42979b6f497bd2e
Taha <ktaha@morni.com>
2025-05-28 13:57:20 +0300

    fix swagger

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,24 +1,3 @@
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-const path = require('path');
 
-const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'My API',
-            version: '1.0.3',
-        },
-    },
-    servers: [
-        {
-            url: 'https://api.docai.online/',
-        },
 
-    ],
-    apis: [path.resolve(__dirname, '../routes/*.js')],
-};
-
-const swaggerSpec = swaggerJSDoc(options);
-
-module.exports = { swaggerUi, swaggerSpec };
+module.exports = { swaggerUi };
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const {swaggerUi} = require("./config/swagger");
+const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 const app = express();
 
