c1e14a71be47cfffbb6b664d6e4011e41eecbcad
Taha <ktaha@morni.com>
2025-06-12 15:51:24 +0300

    add image uploader

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: 'dijppj2e6',
-    api_key: '112173768875596',
-    api_secret: 'zQI9gYq8mdEPI3zed4lz2PQboEQ',
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
