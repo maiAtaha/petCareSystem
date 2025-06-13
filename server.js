0af819fe30cc22be538b093a21b1e78bcb9bd0d0
Taha <ktaha@morni.com>
2025-06-13 13:40:53 +0300

    fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger.json');
+const swaggerDocument = require('./swagger.json');
 const app = express();
 
 app.use('/docs', (req, res, next) => {
